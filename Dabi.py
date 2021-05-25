66755127ddc439b67671653aa0f2d6a61cc3bda8
NAMKUNGDABI <74096581+NAMKUNGDABI@users.noreply.github.com>
2021-05-26 07:48:31 +0900

    Update Dabi.py
    ~evaluation

--- Dabi.py
+++ Dabi.py
@@ -1,19 +1,21 @@
 # Import Libraries
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import OrdinalEncoder
+import category_encoders as ce
+from sklearn.feature_selection import f_classif
+from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier
 from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import preprocessing
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import RobustScaler
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import MaxAbsScaler
-import category_encoders as ce
 from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
-from sklearn.ensemble import ExtraTreesClassifier
+from imblearn.over_sampling import SMOTE
+
 
 # Read the dataset
 data = pd.read_csv("C:/Users/BEE/Desktop/데이터과학/train_strokes.csv")
@@ -170,6 +172,7 @@ print("\n############ Before encoding ############")
 print(data.head())
 print(data.info())
 
+data2 = data.copy()
 target = data["stroke"].copy()
 data = data.iloc[:, 0:-1]
 
@@ -322,32 +325,326 @@ extra_tree = ExtraTreesClassifier()
 extra_tree.fit(data_rb, target)
 feature_imp = pd.Series(extra_tree.feature_importances_, index=data_rb.columns)
 feature_imp.nlargest(11).plot(kind='barh')
-plt.show()
+#plt.show()
 
 
 # ordinal + standard
+extra_tree = ExtraTreesClassifier()
 extra_tree.fit(data_ss, target)
 feature_imp = pd.Series(extra_tree.feature_importances_, index=data_ss.columns)
 feature_imp.nlargest(11).plot(kind='barh')
-plt.show()
+#plt.show()
 
 
 # ordinal + minmax
+extra_tree = ExtraTreesClassifier()
 extra_tree.fit(data_mm, target)
 feature_imp = pd.Series(extra_tree.feature_importances_, index=data_mm.columns)
 feature_imp.nlargest(11).plot(kind='barh')
-plt.show()
+#plt.show()
 
 
 # ordinal + maxabs
+extra_tree = ExtraTreesClassifier()
 extra_tree.fit(data_ma, target)
 feature_imp = pd.Series(extra_tree.feature_importances_, index=data_ma.columns)
 feature_imp.nlargest(11).plot(kind='barh')
-plt.show()
+#plt.show()
 
 
 #heatmap
-m_data = data.drop(['id'],axis=1)
+m_data = data2.drop(['id'],axis=1)
+print(m_data)
 corr_matrix = m_data.corr()
-heatmap = sns.heatmap(data[corr_matrix.index].corr(), annot=True)
-plt.show()
+print(corr_matrix)
+heatmap = sns.heatmap(data2[corr_matrix.index].corr(), annot=True)
+#plt.show()
+
+
+
+data_rb = data_rb.drop(['gender', 'id', 'Residence_type', 'work_type'], axis=1)
+data_ss = data_ss.drop(['gender', 'id', 'Residence_type', 'work_type'], axis=1)
+data_mm = data_mm.drop(['gender', 'id', 'Residence_type', 'work_type'], axis=1)
+data_ma = data_ma.drop(['gender', 'id', 'Residence_type', 'work_type'], axis=1)
+
+
+# split train and test data
+rb_x_train, rb_x_test, rb_y_train, rb_y_test = train_test_split(data_rb, target, test_size=0.2, shuffle=True,
+                                                    stratify=target, random_state=123)
+ss_x_train, ss_x_test, ss_y_train, ss_y_test = train_test_split(data_ss, target, test_size=0.2, shuffle=True,
+                                                    stratify=target, random_state=123)
+mm_x_train, mm_x_test, mm_y_train, mm_y_test = train_test_split(data_mm, target, test_size=0.2, shuffle=True,
+                                                    stratify=target, random_state=123)
+ma_x_train, ma_x_test, ma_y_train, ma_y_test = train_test_split(data_ma, target, test_size=0.2, shuffle=True,
+                                                    stratify=target, random_state=123)
+
+
+log = LogisticRegression(solver='saga')
+knn = KNeighborsClassifier(n_neighbors=7)
+gbc = GradientBoostingClassifier(learning_rate=0.8, n_estimators=7)
+
+
+# Robust scaling data
+print("<<<<<<<<<<<<<Robust scaled data fitting>>>>>>>>>>>>>>\n")
+
+# fitting
+log.fit(rb_x_train, rb_y_train)
+knn.fit(rb_x_train, rb_y_train)
+gbc.fit(rb_x_train, rb_y_train)
+
+# predict
+log_predict = log.predict(rb_x_test)
+knn_predict = knn.predict(rb_x_test)
+gbc_predict = gbc.predict(rb_x_test)
+
+# accuracy
+print("Accuracy of Logistic: " ,accuracy_score(rb_y_test, log_predict))
+print("Accuracy of KNN: ", accuracy_score(rb_y_test, knn_predict))
+print("Accuracy of GBC: ", accuracy_score(rb_y_test, gbc_predict))
+print("\n")
+
+#cross validation
+#log
+dist_log = dict(C=stats.uniform(loc=0, scale=4), penalty=['l2', 'l1'])
+rand_search_log = RandomizedSearchCV(log, param_distributions=dist_log, scoring='accuracy',
+                                 return_train_score=True, n_jobs=-1)
+
+kfold_score = cross_val_score(log, data_rb, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_log.fit(rb_x_train, rb_y_train)
+rs_predict = rand_search_log.predict(rb_x_test)
+rs_accuracy = accuracy_score(rb_y_test, rs_predict)
+
+# log accuracy
+print("After cross validation\n")
+print("Accuracy(Logistic & K-Fold) : ", kfold_accuracy)
+print("Accuracy(Logistic & Rand Search) : ", rs_accuracy)
+print("Best Parameter of Logistic : ", rand_search_log.best_params_)
+
+#knn
+dist_knn = {'n_neighbors': range(3, 21), 'metric': ['euclidean', 'manhattan']}
+rand_search_knn = RandomizedSearchCV(knn, param_distributions=dist_knn, scoring='accuracy',
+                                 return_train_score=True, n_jobs=-1)
+
+kfold_score = cross_val_score(knn, data_rb, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_knn.fit(rb_x_train, rb_y_train)
+rs_predict = rand_search_knn.predict(rb_x_test)
+rs_accuracy = accuracy_score(rb_y_test, rs_predict)
+
+# knn accuracy
+print("\nAccuracy(KNN & K-Fold) : ", kfold_accuracy)
+print("Accuracy(KNN & Rand Search) : ", rs_accuracy)
+print("Best Parameter of KNN : ", rand_search_knn.best_params_)
+
+#gbc
+dist_gbc = {'n_estimators': range(60, 181),
+              'max_depth': [5, 10, 15, 20, 25],
+              'learning_rate': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+              'criterion': ['friedman_mse', 'mse']}
+rand_search_gbc = RandomizedSearchCV(gbc, param_distributions=dist_gbc, scoring='accuracy',
+                                 return_train_score=True, n_jobs=-1)
+
+
+kfold_score = cross_val_score(gbc, data_rb, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_gbc.fit(rb_x_train, rb_y_train)
+rs_predict = rand_search_gbc.predict(rb_x_test)
+rs_accuracy = accuracy_score(rb_y_test, rs_predict)
+
+# gbc accuracy
+print("\nAccuracy(GBC & K-Fold) : ", kfold_accuracy)
+print("Accuracy(GBC & Rand Search) : ", rs_accuracy)
+print("Best Parameter of GBC : ", rand_search_gbc.best_params_)
+
+
+###################################################################
+
+# Standard scaling data
+print("\n\n<<<<<<<<<<<<Standard scaled data fitting>>>>>>>>>>>>\n")
+
+# fitting
+log.fit(ss_x_train, ss_y_train)
+knn.fit(ss_x_train, ss_y_train)
+gbc.fit(ss_x_train, ss_y_train)
+
+# predict
+log_predict = log.predict(ss_x_test)
+knn_predict = knn.predict(ss_x_test)
+gbc_predict = gbc.predict(ss_x_test)
+
+# accuracy
+print("Accuracy of Logistic: " ,accuracy_score(ss_y_test, log_predict))
+print("Accuracy of KNN: ", accuracy_score(ss_y_test, knn_predict))
+print("Accuracy of GBC: ", accuracy_score(ss_y_test, gbc_predict))
+print("\n")
+
+#cross validation
+#log
+kfold_score = cross_val_score(log, data_ss, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_log.fit(ss_x_train, ss_y_train)
+rs_predict = rand_search_log.predict(ss_x_test)
+rs_accuracy = accuracy_score(ss_y_test, rs_predict)
+
+# log accuracy
+print("After cross validation\n")
+print("Accuracy(Logistic & K-Fold) : ", kfold_accuracy)
+print("Accuracy(Logistic & Rand Search) : ", rs_accuracy)
+print("Best Parameter of Logistic : ", rand_search_log.best_params_)
+
+#knn
+kfold_score = cross_val_score(knn, data_ss, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_knn.fit(ss_x_train, ss_y_train)
+rs_predict = rand_search_knn.predict(ss_x_test)
+rs_accuracy = accuracy_score(ss_y_test, rs_predict)
+
+# knn accuracy
+print("\nAccuracy(KNN & K-Fold) : ", kfold_accuracy)
+print("Accuracy(KNN & Rand Search) : ", rs_accuracy)
+print("Best Parameter of KNN : ", rand_search_knn.best_params_)
+
+#gbc
+kfold_score = cross_val_score(gbc, data_ss, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_gbc.fit(ss_x_train, ss_y_train)
+rs_predict = rand_search_gbc.predict(ss_x_test)
+rs_accuracy = accuracy_score(ss_y_test, rs_predict)
+
+# gbc accuracy
+print("\nAccuracy(GBC & K-Fold) : ", kfold_accuracy)
+print("Accuracy(GBC & Rand Search) : ", rs_accuracy)
+print("Best Parameter of GBC : ", rand_search_gbc.best_params_)
+
+
+###################################################################
+
+# MinMax scaling data
+print("\n\n<<<<<<<<<<<<<<MinMax scaled data fitting>>>>>>>>>>>>>")
+
+# fitting
+log.fit(mm_x_train, mm_y_train)
+knn.fit(mm_x_train, mm_y_train)
+gbc.fit(mm_x_train, mm_y_train)
+
+# predict
+log_predict = log.predict(mm_x_test)
+knn_predict = knn.predict(mm_x_test)
+gbc_predict = gbc.predict(mm_x_test)
+
+# accuracy
+print("Accuracy of Logistic: " ,accuracy_score(mm_y_test, log_predict))
+print("Accuracy of KNN: ", accuracy_score(mm_y_test, knn_predict))
+print("Accuracy of GBC: ", accuracy_score(mm_y_test, gbc_predict))
+print("\n")
+
+#cross validation
+#log
+kfold_score = cross_val_score(log, data_mm, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_log.fit(mm_x_train, mm_y_train)
+rs_predict = rand_search_log.predict(mm_x_test)
+rs_accuracy = accuracy_score(mm_y_test, rs_predict)
+
+# log accuracy
+print("After cross validation\n")
+print("Accuracy(Logistic & K-Fold) : ", kfold_accuracy)
+print("Accuracy(Logistic & Rand Search) : ", rs_accuracy)
+print("Best Parameter of Logistic : ", rand_search_log.best_params_)
+
+#knn
+kfold_score = cross_val_score(knn, data_mm, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_knn.fit(mm_x_train, mm_y_train)
+rs_predict = rand_search_knn.predict(mm_x_test)
+rs_accuracy = accuracy_score(mm_y_test, rs_predict)
+
+# knn accuracy
+print("\nAccuracy(KNN & K-Fold) : ", kfold_accuracy)
+print("Accuracy(KNN & Rand Search) : ", rs_accuracy)
+print("Best Parameter of KNN : ", rand_search_knn.best_params_)
+
+#gbc
+kfold_score = cross_val_score(gbc, data_mm, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_gbc.fit(mm_x_train, mm_y_train)
+rs_predict = rand_search_gbc.predict(mm_x_test)
+rs_accuracy = accuracy_score(mm_y_test, rs_predict)
+
+# gbc accuracy
+print("\nAccuracy(GBC & K-Fold) : ", kfold_accuracy)
+print("Accuracy(GBC & Rand Search) : ", rs_accuracy)
+print("Best Parameter of GBC : ", rand_search_gbc.best_params_)
+
+
+###################################################################
+
+# MaxAbs scaling data
+print("\n\n<<<<<<<<<<<<<<<MaxAbs scaled data fitting>>>>>>>>>>>>>>>>\n")
+
+# fitting
+log.fit(ma_x_train, ma_y_train)
+knn.fit(ma_x_train, ma_y_train)
+gbc.fit(ma_x_train, ma_y_train)
+
+# predict
+log_predict = log.predict(ma_x_test)
+knn_predict = knn.predict(ma_x_test)
+gbc_predict = gbc.predict(ma_x_test)
+
+# accuracy
+print("Accuracy of Logistic: " ,accuracy_score(ma_y_test, log_predict))
+print("Accuracy of KNN: ", accuracy_score(ma_y_test, knn_predict))
+print("Accuracy of GBC: ", accuracy_score(ma_y_test, gbc_predict))
+print("\n")
+
+#cross validation
+#log
+kfold_score = cross_val_score(log, data_ma, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_log.fit(ma_x_train, ma_y_train)
+rs_predict = rand_search_log.predict(ma_x_test)
+rs_accuracy = accuracy_score(ma_y_test, rs_predict)
+
+# log accuracy
+print("After cross validation\n")
+print("Accuracy(Logistic & K-Fold) : ", kfold_accuracy)
+print("Accuracy(Logistic & Rand Search) : ", rs_accuracy)
+print("Best Parameter of Logistic : ", rand_search_log.best_params_)
+
+#knn
+kfold_score = cross_val_score(knn, data_ma, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_knn.fit(ma_x_train, ma_y_train)
+rs_predict = rand_search_knn.predict(ma_x_test)
+rs_accuracy = accuracy_score(ma_y_test, rs_predict)
+
+# knn accuracy
+print("\nAccuracy(KNN & K-Fold) : ", kfold_accuracy)
+print("Accuracy(KNN & Rand Search) : ", rs_accuracy)
+print("Best Parameter of KNN : ", rand_search_knn.best_params_)
+
+#gbc
+kfold_score = cross_val_score(gbc, data_ma, target, cv=7)
+kfold_accuracy = np.mean(kfold_score)
+
+rand_search_gbc.fit(ma_x_train, ma_y_train)
+rs_predict = rand_search_gbc.predict(ma_x_test)
+rs_accuracy = accuracy_score(ma_y_test, rs_predict)
+
+# gbc accuracy
+print("\nAccuracy(GBC & K-Fold) : ", kfold_accuracy)
+print("Accuracy(GBC & Rand Search) : ", rs_accuracy)
+print("Best Parameter of GBC : ", rand_search_gbc.best_params_)
